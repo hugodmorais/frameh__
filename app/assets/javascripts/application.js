d4b6324a4a0fc647ecf726c7e84b92300e40c236
hugodmorais <hugodanielmorais@gmail.com>
2018-11-29 21:26:46 +0000

    Integracao bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
