2573c1d88db9f589bb2434ab7c76139bfc244f81
hugodmorais <hugodanielmorais@gmail.com>
2018-11-29 22:35:26 +0000

    Paginas estaticas, views: sobre/contacto/index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,10 @@ class StaticPagesController < ApplicationController
   def index
     @nome = "Hugo"
   end
+
+  def sobre
+  end
+
+  def contato  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
+
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contacto', to: 'static_pages#contacto'
 end
