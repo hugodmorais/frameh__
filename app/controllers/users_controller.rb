1dd111c4d6662086c525ee2f0ccdc666f81ab8cc
hugodmorais <hugodanielmorais@gmail.com>
2018-11-30 22:20:07 +0000

    Validacoes, apresentar erros ao usuario e mensagens de sucesso

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Usuario cadastrado com sucesso'
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   private
 
-  
+  def user_params
+    params.require(:user).permit!
+  end
+
 end
