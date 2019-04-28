b302a60de0c3327160db1c45b97345b450fb3466
Hugo Morais <hugodanielmorais@gmail.com>
2019-04-28 23:42:04 +0100

    minor fix

--- config/puma.rb
+++ config/puma.rb
@@ -13,3 +13,9 @@ on_worker_boot do
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
+ on_worker_boot do
+   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
