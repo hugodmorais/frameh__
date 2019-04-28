d4863ac3c6636f77e042c3854b337d8e12b2b97e
Hugo Morais <hugodanielmorais@gmail.com>
2019-04-28 23:15:33 +0100

    minor fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-Date::MONTHNAMES = [nil] + %w(Janeiro Fevereiro Março Abril Maio Junho Julho Agosto Setembro Outubro Novembro Dezembro)
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::DEBUG
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
