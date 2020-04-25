3d44422d57562270de79fa6f0359b636cc95bad2
hugodmorais <hugodanielmorais@gmail.com>
2020-04-25 14:51:41 +0100

    Nav and sidebar H1

--- app/assets/javascripts/dashboard.coffee
+++ app/assets/javascripts/dashboard.coffee
@@ -2,6 +2,7 @@ dashboard =
   index: ->
     $('.carousel').carousel()
 
+    $('.navbar h1').removeClass('hidden')
     # Draw Events chart
     results_total = new ResultChart('results_total')
     results_total.dashboard_index()
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "dashboard#index"
-  get 'dashboard', to: 'dashboard#index'
+  root 'dashboard#index'
+  get 'dashboard/index', as: 'dashboard'
+
+  # root to: "dashboard#index"
+  # get 'dashboard', to: 'dashboard#index'
   get 'testes', to: 'dashboard#testes'
 
   get 'index', to: 'static_pages#index'
