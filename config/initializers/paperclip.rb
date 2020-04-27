3a8b9ef37b045f8a912e2379f59ccb16d0d894be
hugodmorais <hugodanielmorais@gmail.com>
2020-04-27 17:29:06 +0100

    minor fix

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,8 @@
-# # config/initializers/paperclip.rb
-# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip.interpolates :paperclip_root do |_attachment, _style|
+  Creds.get(:paperclip_path).presence || Rails.root.join('storage', Rails.env)
+end
 
-# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options.merge!(
+  path: ':paperclip_root/:class/:attachment/:id_partition/:basename-:style:dotextension',
+  default_url: 'default/:class/:style/default.jpg'
+)
