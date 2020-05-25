0540e92d791090e6cdee09b9114f844f3d7c579e
hugodmorais <hugodanielmorais@gmail.com>
2020-05-25 22:57:47 +0100

    remove import inside incomes model

--- app/models/income.rb
+++ app/models/income.rb
@@ -46,7 +46,6 @@ class Income < ApplicationRecord
   validates :income_value, :month, presence: true
 
   # Scopes   
-  scope :in_month, ->(month) { where month: month } 
   scope :in_year, ->(year) { joins(:annual_management).where(annual_managements: { year: year }) }
   scope :result_by_year, ->(user) { where(annual_management: AnnualManagement.where(year: Current.year)).where(user_id: user).sum('income_value') }
   
@@ -57,41 +56,6 @@ class Income < ApplicationRecord
   # Default  
   def to_s
       name
-  end
-
-  def self.import(file, user)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      income_category = row["Categoria"]
-      income_company = row["Empresa"]
-      income_user = row["Utilizador"]
-      
-      (1..12).each do |month|
-        income_value = row[I18n.t Date::MONTHNAMES[month]]
-        
-        next if income_value.blank?
-        
-        income = Income.new(month: month, annual_management: AnnualManagement.find_by(year: Current.year)) 
-        income.user_id = user.id
-        income.user_group = UserGroup.find_by(name: income_user)
-        income.company = Company.find_by(name: income_company)
-        income.income_category = IncomeCategory.find_by(name: income_category)
-        income.income_value = income_value
-
-        income.save!
-        
-      end
-    end
-  end
-
-  def self.open_spreadsheet
-    case File.extname(self.file.original_filename)
-    when ".xls" then Roo::Excel.new(self.file.path)
-    when ".xlsx" then Roo::Excelx.new(self.file.path)
-    else raise "Unknown file type:"
-    end
   end
   
   private
